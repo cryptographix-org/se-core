e13c2d1f3935e3d49bf5e7004be9e6738e5423db
SeanWykes <sean.nascent@nascent.com.br>
2015-07-25 00:03:15 -0300

    rebuild

--- dist/commonjs/se-core.d.ts
+++ dist/commonjs/se-core.d.ts
@@ -1,85 +1,5 @@
 declare module 'se-core' {
 
-export enum ISO7816 {
-    CLA_ISO = 0,
-    INS_EXTERNAL_AUTHENTICATE = 130,
-    INS_GET_CHALLENGE = 132,
-    INS_INTERNAL_AUTHENTICATE = 136,
-    INS_SELECT_FILE = 164,
-    INS_READ_RECORD = 178,
-    INS_UPDATE_RECORD = 220,
-    INS_VERIFY = 32,
-    INS_BLOCK_APPLICATION = 30,
-    INS_UNBLOCK_APPLICATION = 24,
-    INS_UNBLOCK_CHANGE_PIN = 36,
-    INS_GET_DATA = 202,
-    TAG_APPLICATION_TEMPLATE = 97,
-    TAG_FCI_PROPRIETARY_TEMPLATE = 165,
-    TAG_FCI_TEMPLATE = 111,
-    TAG_AID = 79,
-    TAG_APPLICATION_LABEL = 80,
-    TAG_LANGUAGE_PREFERENCES = 24365,
-    TAG_APPLICATION_EFFECTIVE_DATE = 24357,
-    TAG_APPLICATION_EXPIRY_DATE = 24356,
-    TAG_CARDHOLDER_NAME = 24352,
-    TAG_ISSUER_COUNTRY_CODE = 24360,
-    TAG_ISSUER_URL = 24400,
-    TAG_PAN = 90,
-    TAG_PAN_SEQUENCE_NUMBER = 24372,
-    TAG_SERVICE_CODE = 24368,
-    ISO_PINBLOCK_SIZE = 8,
-    APDU_LEN_LE_MAX = 256,
-    SW_SUCCESS = 36864,
-    SW_WARNING_NV_MEMORY_UNCHANGED = 25088,
-    SW_PART_OF_RETURN_DATA_CORRUPTED = 25217,
-    SW_END_FILE_REACHED_BEFORE_LE_BYTE = 25218,
-    SW_SELECTED_FILE_INVALID = 25219,
-    SW_FCI_NOT_FORMATTED_TO_ISO = 25220,
-    SW_WARNING_NV_MEMORY_CHANGED = 25344,
-    SW_FILE_FILLED_BY_LAST_WRITE = 25473,
-    SW_WRONG_LENGTH = 26368,
-    SW_FUNCTIONS_IN_CLA_NOT_SUPPORTED = 26624,
-    SW_LOGICAL_CHANNEL_NOT_SUPPORTED = 26753,
-    SW_SECURE_MESSAGING_NOT_SUPPORTED = 26754,
-    SW_COMMAND_NOT_ALLOWED = 26880,
-    SW_COMMAND_INCOMPATIBLE_WITH_FILE_STRUCTURE = 27009,
-    SW_SECURITY_STATUS_NOT_SATISFIED = 27010,
-    SW_FILE_INVALID = 27011,
-    SW_DATA_INVALID = 27012,
-    SW_CONDITIONS_NOT_SATISFIED = 27013,
-    SW_COMMAND_NOT_ALLOWED_AGAIN = 27014,
-    SW_EXPECTED_SM_DATA_OBJECTS_MISSING = 27015,
-    SW_SM_DATA_OBJECTS_INCORRECT = 27016,
-    SW_WRONG_PARAMS = 27136,
-    SW_WRONG_DATA = 27264,
-    SW_FUNC_NOT_SUPPORTED = 27265,
-    SW_FILE_NOT_FOUND = 27266,
-    SW_RECORD_NOT_FOUND = 27267,
-    SW_NOT_ENOUGH_SPACE_IN_FILE = 27268,
-    SW_LC_INCONSISTENT_WITH_TLV = 27269,
-    SW_INCORRECT_P1P2 = 27270,
-    SW_LC_INCONSISTENT_WITH_P1P2 = 27271,
-    SW_REFERENCED_DATA_NOT_FOUND = 27272,
-    SW_WRONG_P1P2 = 27392,
-    SW_INS_NOT_SUPPORTED = 27904,
-    SW_CLA_NOT_SUPPORTED = 28160,
-    SW_UNKNOWN = 28416,
-}
-
-export class CardApplication {
-    onAPDUResponse: any;
-    constructor(onAPDUResponse: any);
-    selectApplication(bP1: any, bP2: any, sAID: any): {
-        sw: number;
-        data: any;
-    };
-    deselectApplication(): void;
-    executeAPDUCommand(bCLA: any, bINS: any, bP1: any, bP2: any, commandData: any, wLe: any): {
-        sw: number;
-        data: any;
-    };
-}
-
 
 export class Key {
     _type: number;
@@ -154,6 +74,10 @@ export class ByteString {
     pad(method: number, optional?: boolean): ByteString;
     toString(encoding: any): string;
 }
+export const HEX: number;
+export const ASCII: number;
+export const BASE64: number;
+export const UTF8: number;
 
 
 export class ByteBuffer {
@@ -196,5 +120,100 @@ export class TLVList {
     _tlvs: TLV[];
     constructor(tlvStream: ByteString, encoding?: number);
     index(index: any): TLV;
+}
+
+export enum ISO7816 {
+    CLA_ISO = 0,
+    INS_EXTERNAL_AUTHENTICATE = 130,
+    INS_GET_CHALLENGE = 132,
+    INS_INTERNAL_AUTHENTICATE = 136,
+    INS_SELECT_FILE = 164,
+    INS_READ_RECORD = 178,
+    INS_UPDATE_RECORD = 220,
+    INS_VERIFY = 32,
+    INS_BLOCK_APPLICATION = 30,
+    INS_UNBLOCK_APPLICATION = 24,
+    INS_UNBLOCK_CHANGE_PIN = 36,
+    INS_GET_DATA = 202,
+    TAG_APPLICATION_TEMPLATE = 97,
+    TAG_FCI_PROPRIETARY_TEMPLATE = 165,
+    TAG_FCI_TEMPLATE = 111,
+    TAG_AID = 79,
+    TAG_APPLICATION_LABEL = 80,
+    TAG_LANGUAGE_PREFERENCES = 24365,
+    TAG_APPLICATION_EFFECTIVE_DATE = 24357,
+    TAG_APPLICATION_EXPIRY_DATE = 24356,
+    TAG_CARDHOLDER_NAME = 24352,
+    TAG_ISSUER_COUNTRY_CODE = 24360,
+    TAG_ISSUER_URL = 24400,
+    TAG_PAN = 90,
+    TAG_PAN_SEQUENCE_NUMBER = 24372,
+    TAG_SERVICE_CODE = 24368,
+    ISO_PINBLOCK_SIZE = 8,
+    APDU_LEN_LE_MAX = 256,
+    SW_SUCCESS = 36864,
+    SW_WARNING_NV_MEMORY_UNCHANGED = 25088,
+    SW_PART_OF_RETURN_DATA_CORRUPTED = 25217,
+    SW_END_FILE_REACHED_BEFORE_LE_BYTE = 25218,
+    SW_SELECTED_FILE_INVALID = 25219,
+    SW_FCI_NOT_FORMATTED_TO_ISO = 25220,
+    SW_WARNING_NV_MEMORY_CHANGED = 25344,
+    SW_FILE_FILLED_BY_LAST_WRITE = 25473,
+    SW_WRONG_LENGTH = 26368,
+    SW_FUNCTIONS_IN_CLA_NOT_SUPPORTED = 26624,
+    SW_LOGICAL_CHANNEL_NOT_SUPPORTED = 26753,
+    SW_SECURE_MESSAGING_NOT_SUPPORTED = 26754,
+    SW_COMMAND_NOT_ALLOWED = 26880,
+    SW_COMMAND_INCOMPATIBLE_WITH_FILE_STRUCTURE = 27009,
+    SW_SECURITY_STATUS_NOT_SATISFIED = 27010,
+    SW_FILE_INVALID = 27011,
+    SW_DATA_INVALID = 27012,
+    SW_CONDITIONS_NOT_SATISFIED = 27013,
+    SW_COMMAND_NOT_ALLOWED_AGAIN = 27014,
+    SW_EXPECTED_SM_DATA_OBJECTS_MISSING = 27015,
+    SW_SM_DATA_OBJECTS_INCORRECT = 27016,
+    SW_WRONG_PARAMS = 27136,
+    SW_WRONG_DATA = 27264,
+    SW_FUNC_NOT_SUPPORTED = 27265,
+    SW_FILE_NOT_FOUND = 27266,
+    SW_RECORD_NOT_FOUND = 27267,
+    SW_NOT_ENOUGH_SPACE_IN_FILE = 27268,
+    SW_LC_INCONSISTENT_WITH_TLV = 27269,
+    SW_INCORRECT_P1P2 = 27270,
+    SW_LC_INCONSISTENT_WITH_P1P2 = 27271,
+    SW_REFERENCED_DATA_NOT_FOUND = 27272,
+    SW_WRONG_P1P2 = 27392,
+    SW_INS_NOT_SUPPORTED = 27904,
+    SW_CLA_NOT_SUPPORTED = 28160,
+    SW_UNKNOWN = 28416,
+}
+
+export class ALU implements Kind {
+    private static $kindInfo;
+    kindInfo: KindInfo;
+    private aluBytes;
+    properties: {};
+    constructor(byteArray?: ByteArray);
+    private getALUSegment(segmentID);
+    static decodeBlob(blob: ByteArray, opts?: Object): ALU;
+    encodeBlob(opts?: {}): ByteArray;
+}
+
+export class MULTOSTester {
+    execTests(): void;
+}
+
+export class CardApplication {
+    onAPDUResponse: any;
+    constructor(onAPDUResponse: any);
+    selectApplication(bP1: any, bP2: any, sAID: any): {
+        sw: number;
+        data: any;
+    };
+    deselectApplication(): void;
+    executeAPDUCommand(bCLA: any, bINS: any, bP1: any, bP2: any, commandData: any, wLe: any): {
+        sw: number;
+        data: any;
+    };
 }
 }
