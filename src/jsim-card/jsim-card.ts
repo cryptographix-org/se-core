1cd04f89af5ee1b9b860e16a591374e3f72d9c8a
SeanWykes <sean.nascent@nascent.com.br>
2015-11-30 19:38:47 -0200

    added JSIMCard interface and renamed card/applet to JSIM_Script

--- src/jsim-card/jsim-card.ts
+++ src/jsim-card/jsim-card.ts
@@ -1,74 +1,15 @@
 import { ByteArray } from 'sim-core';
 
-import { ISO7816 } from '../base/ISO7816';
-import { Slot } from '../base/slot';
 import { CommandAPDU } from '../base/command-apdu';
 import { ResponseAPDU } from '../base/response-apdu';
 
-import { JSIMApplet } from './jsim-applet';
-
-export class JSIMCard implements Slot
+export interface JSIMCard
 {
-  private powerIsOn: boolean;
-  private atr: ByteArray;
+  isPowered: boolean;
 
-  applets: { aid: ByteArray, applet: JSIMApplet }[];
+  powerOn(): Promise<ByteArray>;
+  powerOff(): Promise<boolean>;
+  reset(): Promise<ByteArray>;
 
-  selectedApplet: JSIMApplet;
-
-  constructor()
-  {
-    this.atr = new ByteArray( [] );
-  }
-
-  loadApplication( aid: ByteArray, applet: JSIMApplet )
-  {
-    this.applets.push( { aid: aid, applet: applet } );
-  }
-
-  get isPresent()
-  {
-    return true;
-  }
-
-  get isPowered(): boolean
-  {
-    return this.powerIsOn;
-  }
-
-  powerOn(): Promise<ByteArray>
-  {
-    return Promise.resolve( this.atr );
-  }
-
-  powerOff(): Promise<any>
-  {
-    return Promise.resolve(  );
-  }
-
-  reset(): Promise<ByteArray>
-  {
-    return Promise.resolve( this.atr );
-  }
-
-  executeAPDU( commandAPDU: CommandAPDU ): Promise<ResponseAPDU>
-  {
-    if ( commandAPDU.properties.INS == 0xA4 )
-    {
-      if ( this.selectedApplet )
-      {
-        this.selectedApplet.deselectApplication();
-
-        this.selectedApplet = undefined;
-      }
-
-      //TODO: Lookup Application
-      this.selectedApplet = this.applets[ 0 ].applet;
-
-      return this.selectedApplet.selectApplication( commandAPDU );
-    }
-
-    return this.selectedApplet.executeAPDU( commandAPDU );
-  }
-
+  exchangeAPDU( commandAPDU: CommandAPDU ): Promise<ResponseAPDU>;
 }
